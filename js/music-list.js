c74cc10e47d90d1b2930ae099efaf6e0ef006bd0
Đặng Ngọc Điệp <104116812+ndiepft215@users.noreply.github.com>
2022-04-24 16:25:22 +0700

    upload

---
+++ js/music-list.js
@@ -0,0 +1,250 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Muộn Rồi Mà Sao Còn",
+    artist: "Sơn Tùng M-TP",
+    img: "music-1",
+    src: "music-1",
+    lyrics: `chưa có lời`
+  },
+  {
+    name: "An Thần",
+    artist: "Thắng, LowG",
+    img: "music-2",
+    src: "music-2",
+    lyrics:`Tại sao lúc vui em bao giờ cũng đang bận?
+    Lúc bên anh bao giờ cũng đang giận?
+    Nói chung anh chỉ là thuốc an thần
+    Với em anh chỉ là thuốc an thần
+    Nói em không nên dùng quá hai lần
+    Nói em chỉ nên dùng lúc em cần
+    Với em anh chỉ là thuốc an thần
+    Với em anh chỉ là, thằng bạn thân
+    Thuốc tây cùng lá cây
+    Nó chỉ làm em quá cao để xuống đây
+    Anh chỉ mơ những đắm say, đắm say
+    Em còn không muốn nắm tay, nắm tay
+    Nói anh nghe chuyện đắng cay, đắng cay
+    Mỗi khi theo người ta đi đó đây cùng gió mây
+    Em tìm anh chỉ khi nó uống say rồi quá tay
+    Cơn giận nguôi rồi mai em sẽ bay, em sẽ bay
+    Tại sao lúc vui em bao giờ cũng đang bận?
+    Lúc bên anh bao giờ cũng đang giận?
+    Nói chung anh chỉ là thuốc an thần
+    Với em anh chỉ là thuốc an thần
+    Nói em không nên dùng quá hai lần
+    Nói em chỉ nên dùng lúc em cần
+    Với em anh chỉ là thuốc an thần
+    Với em anh chỉ là thuốc an thần
+    Với em anh chỉ là...
+    Đi tìm anh khi hơi toàn men
+    Em lại đi kể về nó với anh ở trên con Audi đen
+    Hôm trước anh mang cây thông từ trên Vincom Bà Triệu buổi đêm
+    Thì em tự nhiên ngồi cạnh lò sưởi xong rồi khóc lóc vì ghen
+    Anh đưa em LV Bag, lúc em buồn em thích nó
+    Đầu bếp anh làm thịt cừu, lúc em buồn em thích nó
+    Em chơi trong biệt thự Vin của anh để nỗi buồn vứt xó
+    Nhưng sáng hôm sau thức dậy thì cái nắm tay cũng không cho
+    Xong lại uh, đẩy anh đi ra xa
+    Bỏ anh ở trong căn phòng toàn nhạc của Frank Sinatra
+    Em quay lại ăn chơi với đám bạn ở Circle K
+    Đi theo mấy thằng rapper ngoài đường uống bia trên hồ Tây
+    Tại sao em cứ đâm đầu vào mấy thằng con trai trong hood
+    Nghệ sĩ nhà tập thể, tuy bất cần nhưng nó cuốn hút
+    Rồi lúc nó chán em theo con khác thì anh lại phải đón em
+    Anh chán nghe chuyện drama nhưng vì em mà phải đón xem
+    Sao lúc vui em bao giờ cũng đang bận?
+    Lúc bên anh bao giờ cũng đang giận?
+    Nói chung anh chỉ là thuốc an thần
+    Với em anh chỉ là thuốc an thần
+    Nói em không nên dùng quá hai lần
+    Nói em chỉ nên dùng lúc em cần
+    Với em anh chỉ là thuốc an thần
+    Với em anh chỉ là thuốc an thần
+    Với em anh chỉ là...
+    Tại sao lúc vui em bao giờ...
+    Tại sao lúc vui em bao giờ...`
+  },
+  {
+    name: "Dạ Vũ - Tăng Duy Tân",
+    artist: "Tăng Duy Tân, Phong Max",
+    img: "music-3",
+    src: "music-3",
+    lyrics:`Khi màn đêm vừa buông
+    Mưa vừa tuôn ở trên mái hiên, em vẫn cứ uống
+    Sương còn vương hàng trăm nỗi đau như kéo em xuống
+    Nơi vực sâu tận cùng trái tim khô cằn sỏi đá
+    Chân trời xa vắng đưa con tim em đi về đâu
+    Sóng xô muuôn trùng, đẩy đưa tháng năm yêu lần đầu
+    Chưa từng cho phép suy tư, em không cho mình đau
+    Nhưng sâu trong thâm tâm trái tim em đang không ngừng nhỏ máu
+    Vì lỡ say trong một vòng tay ai
+    Dành cả tương lai đổi lấy nỗi buồn
+    Từ đó em không còn cần ai thương
+    Giọt sầu ai vương, nước mắt em tuôn, giữa đêm dài
+    Màn đêm ôm lấy em
+    Giữ em trong giấc mộng ngày còn ấm êm
+    Một hàng mi lấm lem
+    Ngả lưng lên nỗi buồn chẳng thể đong đếm
+    Chân trời xa vắng đưa con tim em đi về đâu
+    Sóng xô muôn trùng, đẩy đưa tháng năm yêu lần đầu
+    Chưa từng cho phép suy tư, em không cho mình đau
+    Nhưng sâu trong thâm tâm trái tim em đang không ngừng nhỏ máu
+    Vì lỡ say trong một vòng tay ai
+    Dành cả tương lai đổi lấy nỗi buồn
+    Từ đó em không còn cần ai thương
+    Giọt sầu ai vương, nước mắt em tuôn, giữa đêm dài`
+  },
+  {
+    name: "Tình Đầu - Tăng Duy Tân",
+    artist: "Tăng Duy Tân",
+    img: "music-4",
+    src: "music-4",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Ngây Thơ",
+    artist: "Tăng Duy Tân x Phong Max",
+    img: "music-5",
+    src: "music-5",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "3107-3",
+    artist: "W/n; Duongg; Nâu; Titie",
+    img: "music-6",
+    src: "music-6",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "3107-2",
+    artist: "W/n; Duongg; Nâu",
+    img: "music-7",
+    src: "music-7",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Loving You Sunny",
+    artist: "Kimmese; Đen",
+    img: "music-8",
+    src: "music-8",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "The Playah (Special Performance)",
+    artist: "Soobin; SlimV",
+    img: "music-9",
+    src: "music-9",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Nước Mắt Em Lau Bằng Tình Yêu Mới",
+    artist: "DaLAB; Tóc Tiên",
+    img: "music-10",
+    src: "music-10",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Ta Và Nàng",
+    artist: "Đen; JGKiD",
+    img: "music-11",
+    src: "music-11",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Ghé Thăm",
+    artist: "Đen, Kimmese, Linh Cáo, JGKiD",
+    img: "music-12",
+    src: "music-12",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Có Chắc Yêu Là Đây",
+    artist: "Sơn Tùng M-TP",
+    img: "music-13",
+    src: "music-13",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Chán Gái 707",
+    artist: "LowG",
+    img: "music-14",
+    src: "music-14",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Crush",
+    artist: "W/n (Ft An An x Vani)",
+    img: "music-15",
+    src: "music-15",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Ai Đưa Em Về",
+    artist: "TIA; Lê Thiện Hiếu",
+    img: "music-16",
+    src: "music-16",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Em Không Thể",
+    artist: "Tiên Tiên; Touliver",
+    img: "music-17",
+    src: "music-17",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Krazy",
+    artist: "Touliver; Binz; Andree; Evy",
+    img: "music-18",
+    src: "music-18",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Làm Người Luôn Yêu Em",
+    artist: "Sơn Tùng M-TP",
+    img: "music-19",
+    src: "music-19",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "S.K.Y.Prox (Original Mix)",
+    artist: "HoaProx",
+    img: "music-20",
+    src: "music-20",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Nến Và Hoa",
+    artist: "Rhymastic",
+    img: "music-21",
+    src: "music-21",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  {
+    name: "Bản Tình Ca Không Hoàn Thiện",
+    artist: "Beat",
+    img: "music-22",
+    src: "music-22",
+    lyrics:"<p>Bài hát này chưa có Lyrics</p>"
+  },
+  
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
